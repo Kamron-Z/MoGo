a10ac05460e6b03890bf1b7eb82da17ca7d4e29e
Kamron-Z <komikomi112020@gmail.com>
2021-03-19 00:57:01 +0500

    second mogo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
-// 
+// @@include('alert.js')
+
+let header = document.querySelector(".header");
+let intro = document.querySelector(".intro").scrollHeight;
+let burger = document.querySelector(".burger")
+let nav = document.querySelector(".nav")
+let wedo = document.querySelector(".accordion__inner");
+
+window.addEventListener("scroll", function() {
+
+  let scrollPos = window.scrollY;
+  
+  if(scrollPos >= intro) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  let scrollPos = window.scrollY;
+  
+  if(scrollPos >= intro) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+burger.addEventListener("click", function() {
+
+  burger.classList.toggle("active");
+  nav.classList.toggle("active");
+});
+
+  wedo.addEventListener("click", function(){
+
+  wedo.classList.toggle("active");
+
+});
